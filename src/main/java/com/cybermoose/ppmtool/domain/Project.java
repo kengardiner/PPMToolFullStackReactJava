1c39b241c139432491dd8aa86ccabdc551e8656d
Ken Gardiner <kagemail@gmail.com>
2019-01-13 10:01:08 -0500

    Fixed directory structure

---
+++ src/main/java/com/cybermoose/ppmtool/domain/Project.java
@@ -0,0 +1,17 @@
+package com.cybermoose.ppmtool.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String projectName;
+    private String
+
+}
