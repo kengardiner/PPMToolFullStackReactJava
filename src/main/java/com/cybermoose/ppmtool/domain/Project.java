5568e8c74d537878317e0784acfc2aa8b00c034a
Ken Gardiner <kagemail@gmail.com>
2019-01-14 19:32:21 -0500

    Project entity added

--- src/main/java/com/cybermoose/ppmtool/domain/Project.java
+++ src/main/java/com/cybermoose/ppmtool/domain/Project.java
@@ -1,9 +1,7 @@
 package com.cybermoose.ppmtool.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 public class Project {
@@ -12,6 +10,72 @@ public class Project {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String projectName;
-    private String
+    private String projectIdentifier;
+    private String description;
+    private Date start_date;
+    private Date end_date;
 
+    private Date created_At;
+    private Date updated_At;
+
+    public Project() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getProjectIdentifier() {
+        return projectIdentifier;
+    }
+
+    public void setProjectIdentifier(String projectIdentifier) {
+        this.projectIdentifier = projectIdentifier;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(Date start_date) {
+        this.start_date = start_date;
+    }
+
+    public Date getEnd_date() {
+        return end_date;
+    }
+
+    public void setEnd_date(Date end_date) {
+        this.end_date = end_date;
+    }
+
+    @PrePersist
+    protected void onCreate () {
+        this.created_At = new Date();
+    }
+
+    @PrePersist
+    protected void onUpdate() {
+        this.updated_At = new Date();
+    }
 }
